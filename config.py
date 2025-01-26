a2d217dc16bdc58c1ec689d3981dc0bef8378af6
ultra121314 <gamerultra064@gmail.com>
2025-01-26 06:51:13 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-BOT_TOKEN = "7424238580:AAEzWYhZx9VslXvGoZ5yaGxfsbU7icfrGL4"
-ADMIN_IDS = ["7210717311"]
-GROUP_ID = -1002399990268  # Replace with your group ID
-GROUP_LINK = "https://t.me/DARKFREEDDOS"  # Replace with your group invite link
-# Default number of threads for attacks
-DEFAULT_THREADS = 900  # Set the default number of threads to use for attacks
